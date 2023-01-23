a4357cf94adc764be019b008a3af520e98b7dea2
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-01-23 09:52:56 +0300

    update test_capitalize

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,10 +1,8 @@
 from pack_funk.capitalize import capitalize
 
 
-if capitalize('hello!') != 'Hello!':
-    raise Exception('The function does not work properly.')
+assert capitalize('hello!') == 'Hello!'
 
-if capitalize('') != '':
-    raise Exception('The function does not work properly.')
+assert capitalize('') == ''
 
 print('All tests passed successfully.')
